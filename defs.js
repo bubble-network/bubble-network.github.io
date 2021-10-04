fee21c8526d211c2416bf7983ae96f2b91792d49
blmayer <bleemayer@gmail.com>
2021-10-04 20:22:39 -0300

    Reverted urls

--- defs.js
+++ defs.js
@@ -1,4 +1,4 @@
-// const apiUrl = "https://api.bubble-network.com/";
-// const wsUrl = "wss://socket.bubble-network.com/ws";
-const apiUrl = "http://localhost:8080/";
-const wsUrl = "ws://localhost:8081/ws";
+const apiUrl = "https://api.bubble-network.com/";
+const wsUrl = "wss://socket.bubble-network.com/ws";
+// const apiUrl = "http://localhost:8080/";
+// const wsUrl = "ws://localhost:8081/ws";
